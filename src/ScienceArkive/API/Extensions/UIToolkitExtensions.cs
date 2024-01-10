cfef1274f31064fb9ac5f48ec0ff674160e78634
Leonardo Ascione <leonardo.ascione@gmail.com>
2024-01-11 00:41:33 +0100

    Fix stop wheel propagation

--- src/ScienceArkive/API/Extensions/UIToolkitExtensions.cs
+++ src/ScienceArkive/API/Extensions/UIToolkitExtensions.cs
@@ -12,25 +12,26 @@ public static class UIToolkitExtensions
     public static GameInstance Game => GameManager.Instance.Game;
 
     /// <summary>
-    ///     Unfortunately, I didn't find a way to stop the propagation of wheel events in UIToolkit to GameInput
+    /// Stop the wheel event from propagating to the game (e.g. zoom)
     /// </summary>
     /// <param name="element"></param>
     public static void StopWheelEventPropagation(this VisualElement element)
     {
-        element.RegisterCallback<PointerEnterEvent>(evt =>
-        {
-            // _Logger.LogInfo(
-            //     $"PointerEnterEvent (flight={Game.Input.Flight.CameraZoom.enabled}, map={Game.Input.MapView.cameraZoom.enabled}, vab={Game.Input.VAB.cameraZoom.enabled})");
-            Game.Input.Flight.CameraZoom.Disable();
-            Game.Input.MapView.cameraZoom.Disable();
-            Game.Input.VAB.cameraZoom.Disable();
-        });
-        element.RegisterCallback<PointerLeaveEvent>(evt =>
-        {
-            // _Logger.LogInfo("PointerLeaveEvent");
-            Game.Input.Flight.CameraZoom.Enable();
-            Game.Input.MapView.cameraZoom.Enable();
-            Game.Input.VAB.cameraZoom.Enable();
-        });
+        element.RegisterCallback<PointerEnterEvent>(OnVisualElementPointerEnter);
+        element.RegisterCallback<PointerLeaveEvent>(OnVisualElementPointerLeave);
+    }
+
+    private static void OnVisualElementPointerEnter(PointerEnterEvent evt)
+    {
+        Game.Input.Flight.CameraZoom.Disable();
+        Game.Input.MapView.cameraZoom.Disable();
+        Game.Input.VAB.cameraZoom.Disable();
+    }
+
+    private static void OnVisualElementPointerLeave(PointerLeaveEvent evt)
+    {
+        Game.Input.Flight.CameraZoom.Enable();
+        Game.Input.MapView.cameraZoom.Enable();
+        Game.Input.VAB.cameraZoom.Enable();
     }
 }
