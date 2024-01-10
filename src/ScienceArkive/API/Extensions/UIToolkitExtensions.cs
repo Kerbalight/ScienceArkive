ddab53b6a0a841dae5a35110c46ac56927a35c60
Leonardo Ascione <leonardo.ascione@gmail.com>
2024-01-11 00:20:56 +0100

    Disable game zoom when hover the GUI

--- src/ScienceArkive/API/Extensions/UIToolkitExtensions.cs
+++ src/ScienceArkive/API/Extensions/UIToolkitExtensions.cs
@@ -1,18 +1,36 @@
-﻿using UnityEngine.UIElements;
+﻿using BepInEx.Logging;
+using KSP.Game;
+using KSP.Input;
+using UnityEngine.UIElements;
 
 namespace ScienceArkive.API.Extensions;
 
 public static class UIToolkitExtensions
 {
+    private static readonly ManualLogSource _Logger = Logger.CreateLogSource("ScienceArkive.UIToolkitExtensions");
+
+    public static GameInstance Game => GameManager.Instance.Game;
+
     /// <summary>
     ///     Unfortunately, I didn't find a way to stop the propagation of wheel events in UIToolkit to GameInput
     /// </summary>
     /// <param name="element"></param>
     public static void StopWheelEventPropagation(this VisualElement element)
     {
-        //element.RegisterCallback<WheelEvent>(evt =>
-        //{
-        //    UitkForKsp2.API.Extensions.DisableGameInputOnFocus
-        //}, TrickleDown.TrickleDown);
+        element.RegisterCallback<PointerEnterEvent>(evt =>
+        {
+            // _Logger.LogInfo(
+            //     $"PointerEnterEvent (flight={Game.Input.Flight.CameraZoom.enabled}, map={Game.Input.MapView.cameraZoom.enabled}, vab={Game.Input.VAB.cameraZoom.enabled})");
+            Game.Input.Flight.CameraZoom.Disable();
+            Game.Input.MapView.cameraZoom.Disable();
+            Game.Input.VAB.cameraZoom.Disable();
+        });
+        element.RegisterCallback<PointerLeaveEvent>(evt =>
+        {
+            // _Logger.LogInfo("PointerLeaveEvent");
+            Game.Input.Flight.CameraZoom.Enable();
+            Game.Input.MapView.cameraZoom.Enable();
+            Game.Input.VAB.cameraZoom.Enable();
+        });
     }
 }
