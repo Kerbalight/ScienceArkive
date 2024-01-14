eca675ca85881c87f58a40753695546c437b2847
Leonardo Ascione <112330494+rockfactory@users.noreply.github.com>
2024-01-14 23:33:49 +0100

    Fix some regions missing in the experiments list

--- src/ScienceArkive/UI/Components/ExperimentSummary.cs
+++ src/ScienceArkive/UI/Components/ExperimentSummary.cs
@@ -83,15 +83,11 @@ public class ExperimentSummary
                     regionEntry.userData = regionController;
                     var regionResearchLocation = new ResearchLocation(true, celestialBody.Name, situation, region.Id);
 
-                    // This double check is not the cleanest. We use:
-                    // - the DataFlavorDescriptions, since we only want to show locations which has been validated by devs
-                    // - the ResearchLocationScalar, since we want to omit negative values (es. Kerbin_Beach_Splashed: technically _there is a flavor text_, even if the science is negative)
+                    // We previously checked the DataFlavorDescriptions, since we only want to show locations which has been validated by devs, but it appears sometimes they're missing
+                    // So we use the ResearchLocationScalar, since we want to omit negative values (es. Kerbin_Beach_Splashed: technically _there is a flavor text_, even if the science is negative)
                     // We should probably double check how KSP2 does it by itself
                     ArchiveManager.Instance.GetResearchLocationScalar(regionResearchLocation, out var scienceScalar);
                     if (scienceScalar < 0f) continue;
-                    var isRegionFlavorPresent = experiment.DataFlavorDescriptions.Any(flavor =>
-                        flavor.ResearchLocationID == regionResearchLocation.ResearchLocationId);
-                    if (!isRegionFlavorPresent) continue;
 
                     regionController.Bind(experiment, regionResearchLocation, reports);
                     content.Add(regionEntry);
